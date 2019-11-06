35339fcd80c8771633ff15871b4d7d948ef70d6e
Chris Kerr <chriskerr@me.com>
2019-11-06 14:43:45 +1100

    minor ActionCable changes for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,10 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+
+  config.web_socket_server_url = "wss://chrskerr-projectone.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://chrskerr-projectone.herokuapp.com', 'http://chrskerr-projectone.herokuapp.com']
+
+
 end
